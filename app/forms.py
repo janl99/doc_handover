1e7c7273dd461966944205b0bb07a2eb978d2a48
janl <janl99@163.com>
2016-06-18 18:29:24 +0800

    add useredit ,changepassword,setting_headimg page.

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,17 @@
 from flask.ext.wtf import Form
-from wtforms import TextField,BooleanField,StringField,TextAreaField
-from wtforms.validators import Required,Email,Length
+from wtforms import TextField,BooleanField,StringField,TextAreaField,PasswordField
+from wtforms.validators import Required,Email,Length,EqualTo
 
 class LoginForm(Form):
     username = TextField('username',validators = [Required()])
     password = TextField('password',validators = [Required()])
 
 class UserEditForm(Form):
-    username = StringField('username', [Required()])
     email = TextField('email', [Required(),Email()])
     about_me = TextAreaField('about me', [Length(min=6,max=256)])
     dynamic = TextField('dynamic',[Length(min=6,max=64)])
+
+class ChangePasswordForm(Form):
+    password = PasswordField('password',validators=[Required()])
+    newpassword = PasswordField('newpassword',validators=[Required(),Length(min=6,max=12)])
+    confimpassword = PasswordField('confimpassword',validators=[Required(),Length(min=6,max=12),EqualTo('newpassword',message='password must be same.')])
--- app/views.py
+++ app/views.py
@@ -3,8 +3,10 @@ from flask import render_template,redirect,session,url_for,request,g,flash
 from flask.ext.login import login_user,logout_user,current_user,login_required
 from app import db,lm 
 from models import User,ROLE_USER,ROLE_ADMIN
-from forms import LoginForm,UserEditForm
+from forms import LoginForm,UserEditForm,ChangePasswordForm
+from hashlib import md5
 
+'''S-flask-login'''
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -12,7 +14,9 @@ def load_user(id):
 @app.before_request
 def before_request():
     g.user = current_user
+'''E-flask-login'''
 
+'''S-index'''
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -20,44 +24,95 @@ def index():
     title = "Genal data tool" 
     user = g.user
     return render_template("index.html",title = title,user=user) 
+'''E-index'''
 
+'''S-login'''
 @app.route('/login',methods =['GET','POST'])
 def login():
     if g.user is not None and g.user.is_authenticated:
         return redirect(flask.url_for('index'))
     form = LoginForm()
+    errors =[]
     if form.validate_on_submit():
+        print "LoginForm Post"
         user = User.query.filter_by(nickname = form.username.data ).first()
-        if user is None and form.user.data == "janl" :
-            user = User(nickname="janl",password = "123456",email="janl@163.com",role = ROLE_ADMIN)
-            db.session.add(user)
-            db.session.commit()
-            return render_template('login.html')
+        if user is None:
+            errors.append('Invalid login. Please try again.')
+            if form.username.data == "janl" :
+                user = User(nickname="janl",password = md5("123456").hexdigest(),email="janl@163.com",role = ROLE_ADMIN)
+                jdb.session.add(user)
+                db.session.commit()
+            return render_template('login.html',form=form,errors=errors)
+        pwmd5code = md5(form.password.data).hexdigest()
         if user is not None:
-            if user.password == form.password.data:
+            if user.password == pwmd5code:
                 login_user(user)
                 flash('logined in successfully.')
                 return redirect(request.args.get('next') or url_for('index'))
-    return render_template('login.html',form=form)
+    return render_template('login.html',form=form,errors=errors)
+'''E-login'''
 
+'''S-logout'''
 @app.route('/logout')
 def logout():
     logout_user()
     user =  current_user
     return redirect(url_for('login'))
+'''E-logout'''
 
+'''S-profile'''
 @app.route('/profile',methods =['GET','POST'])
 @login_required
 def profile():
-    userform = UserEditForm()
-    print userform
-    if userform.validate_on_submit():
-        print userform.email.data
-        print userform.dynamic.data
-        print userform.about_me.data
-        g.user.email = userform.email.data
-        g.user.dynamic = userform.dynamic.data
-        g.user.about_me = userform.about_me.data
+    return render_template('profile.html',user=g.user)
+'''E-profile'''
+
+'''S-useredit'''
+@app.route('/useredit',methods=['GET','POST'])
+@login_required
+def useredit():
+    form = UserEditForm()
+    if form.validate_on_submit():
+        print "UserEditForm Post"
+        print form.email.data
+        print form.dynamic.data
+        print form.about_me.data
+        g.user.email = form.email.data
+        g.user.dynamic = form.dynamic.data
+        g.user.about_me = form.about_me.data
         db.session.add(g.user)
         db.session.commit()
-    return render_template('profile.html',user=g.user,userform=userform)
+        return redirect(url_for('profile'))
+    else:
+        form.email.data = g.user.email
+        form.dynamic.data = g.user.dynamic
+        form.about_me.data = g.user.about_me
+    return render_template('useredit.html',form=form,user=g.user)
+'''E-useredit'''
+
+'''S-changepassword'''
+@app.route('/changepassword',methods=['GET','POST'])
+@login_required
+def changepassword():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        password = form.password.data
+        newpassword = form.newpassword.data
+        confimpassword = form.confimpassword.data
+        print g.user.password
+        pwmd5code = md5(password).hexdigest()
+        print pwmd5code
+        if g.user.password == pwmd5code: 
+            g.user.password = md5(newpassword).hexdigest()
+            db.session.add(g.user)
+            db.session.commit()
+            return redirect(url_for('profile'))
+    return render_template('changepassword.html',form=form,user=g.user)
+'''E-changepassword'''
+
+'''S-setting_headimg'''
+@app.route('/setting_headimg',methods=['GET','POST'])
+@login_required
+def setting_headimg():
+    return render_template('setting_headimg.html',user=g.user)
+'''E-setting_headimg'''
